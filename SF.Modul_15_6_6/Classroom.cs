af1b79b414073992caa376538aacc38349ad1cf1
Irina Lokosova <persik2466@gmail.com>
2021-12-14 23:50:00 +0400

    Добавьте файлы проекта.

---
+++ SF.Modul_15_6_6/Classroom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SF.Modul_15_6_6
+{  
+    /// <summary>
+    /// Список классов
+    /// </summary>
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+}
